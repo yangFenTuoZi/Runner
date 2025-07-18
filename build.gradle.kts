4a6930d0bbfda4000ec729fbdcad97c13f5e9065
真芙辣 <140885477+yangFenTuoZi@users.noreply.github.com>
2025-07-18 11:56:54 +0800

    更新依赖、目标SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("dev.rikka.tools.materialthemebuilder") version "1.5.1"
 }
 
@@ -10,9 +10,9 @@ val gitCommitCount: Int = listOf("git", "rev-list", "--count", "HEAD").execute(p
 val baseVersionName = "1.0.0-rc1"
 
 extra.apply {
-    set("compileSdk", 35)
+    set("compileSdk", 36)
     set("minSdk", 24)
-    set("targetSdk", 35)
+    set("targetSdk", 36)
     set("versionCode", gitCommitCount)
     set("versionName", "${baseVersionName}.r${gitCommitCount}.${gitCommitId}")
 }
