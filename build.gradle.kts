99c8889ded18968048e83cdaf4cfeb76f3ae617a
yangFenTuoZi <140885477+yangFenTuoZi@users.noreply.github.com>
2025-10-18 18:23:06 +0800

    build: 升级 Gradle 及插件版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.12.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("com.android.application") version "8.13.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("dev.rikka.tools.materialthemebuilder") version "1.5.1" apply false
     id("dev.rikka.tools.refine") version "4.4.0" apply false
 }
